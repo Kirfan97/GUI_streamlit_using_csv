7b700a78afea430302ce3d0c2583e75550636b60
Kirfan97 <alongirfan97@gmail.com>
2024-09-29 22:04:06 +0800

    different csv

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 
-data=pd.read_csv("2021_Cars_Aggregated.csv")
+data=pd.read_csv("2021_Vans_Aggregated.csv")
 
 
 
@@ -11,6 +11,7 @@ highest=max(data['OBFCM Fuel consumption (l/100 km)'])
 values = st.slider('Select a range of values',lowest, highest, (lowest, highest))
 lval=values[0]
 hval=values[1]
+
 
 filtered_df = data.loc[(data['OBFCM Fuel consumption (l/100 km)'] <= hval) & (data['OBFCM Fuel consumption (l/100 km)'] >= lval) ]
 st.scatter_chart(data=filtered_df, x='OBFCM Fuel consumption (l/100 km)', y='Manufacturer',color='Fuel Type')
